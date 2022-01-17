63fc1a02dff3d6bd062559bbf1e62a2b733453a3
fahim-shahriar-v1 <f.sm151496@gmail.com>
2022-01-17 12:27:50 +0600

    Initial commit

---
+++ app/src/main/java/com/fahim/testing_demo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.fahim.testing_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    companion object {
+        var FLIGHT_PROMOTION_IMAGE = ""
+    }
+}
---
+++ app/src/main/java/com/fahim/testing_demo/backstacktest/TestFragment.kt
@@ -0,0 +1,15 @@
+package com.fahim.testing_demo.backstacktest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fahim.testing_demo.R
+class TestFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
